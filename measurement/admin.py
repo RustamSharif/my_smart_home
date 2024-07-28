c05d587d27a4d13273e1cea9e5a4fa9ae1e8d285
Rustam Sharipov <r.z.sharipov@gmail.com>
2024-07-29 01:49:46 +0500

    2

--- measurement/admin.py
+++ measurement/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Sensor, Measurement
 
-# Register your models here.
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')  # отображаемые поля в списке админки
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sensor', 'temperature', 'created_at')  # отображаемые поля в списке админки
--- measurement/urls.py
+++ measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import SensorListCreateView, SensorRetrieveUpdateView, SensorDetailView, MeasurementCreateView, MeasurementListView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorListCreateView.as_view(), name='sensor-list-create'),
+    path('sensors/<int:pk>/', SensorRetrieveUpdateView.as_view(), name='sensor-retrieve-update'),
+    path('sensors/detail/<int:pk>/', SensorDetailView.as_view(), name='sensor-detail'),
+    path('measurements/', MeasurementCreateView.as_view(), name='measurement-create'),
+    path('measurements/list/', MeasurementListView.as_view(), name='measurement-list')  # Добавлен маршрут для списка измерений
 ]
